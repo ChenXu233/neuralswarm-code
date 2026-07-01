590084ffff888e40c7f855119987a567b11bde6a
XuChenXu <91937041+ChenXu233@users.noreply.github.com>
2026-07-01 16:30:26 +0800

    feat(core): add PluginSlot generic slot renderer

---
+++ client/src/core/plugin-slot.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import type { SlotName } from './types'
+import { getSlotRegistrations } from './plugin-registry'
+
+const props = defineProps<{
+  name: SlotName
+}>()
+
+const registrations = computed(() => getSlotRegistrations(props.name))
+</script>
+
+<template>
+  <template v-for="reg in registrations" :key="reg.id">
+    <component :is="reg.component" v-bind="$attrs" />
+  </template>
+</template>
